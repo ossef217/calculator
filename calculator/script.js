fa05c20321c51459de8ca04f80b4ceb9d383a28b
Youssef Elhaimoudi <42881723+ossef217@users.noreply.github.com>
2022-04-12 19:02:29 -0400

    Add files via upload
    Calculator using JS/HTML/CSS

---
+++ calculator/script.js
@@ -0,0 +1,31 @@
+// Get Buttons and Input Values
+const userNumbers = document.querySelectorAll('button');
+const output = document.querySelector('.output');
+
+// Calculate() function using eval() method
+function calculatorHandler(event) {
+  const buttValue = event.target.value;
+  if (buttValue === '=') {
+  
+    if (output.value !== '') 
+    {
+      try {
+        output.value = eval(output.value);
+      }
+      catch (error) {
+        alert("Invalid Input");
+      }
+      console.log(eval(output.value));
+    }
+  } else if (buttValue === 'C') 
+  {
+    output.value = '';
+  } else {
+    output.value += buttValue;
+  }
+}
+
+// Add an event listener to a specific element(button)
+userNumbers.forEach(function(uNumber) {
+    uNumber.addEventListener('click', calculatorHandler);
+});
